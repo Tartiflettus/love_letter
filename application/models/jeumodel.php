ae2c8dd0c23c211eacc28036acae3b43fbdb47a3
Creusel <victor.creusel4@etu.univ-lorraine.fr>
2016-11-13 21:25:37 +0100

    Ajout de fonctions de pioche dans le modèle. Non testé

--- application/models/jeumodel.php
+++ application/models/jeumodel.php
@@ -8,6 +8,26 @@
 
 class JeuModel extends CI_Model {
     function getTaillePioche(){
+        $this->db->query("select count(*) from cartes where statut='pioche'");
+    }
 
+    function piocheEstVide(){
+        return $this->getTaillePioche() == 0;
+    }
+
+    //piocher une carte dans la pioche
+    function piocher($idJoueur){
+        assert(isset($idJoueur) && is_numeric($idJoueur) && 0 <= $idJoueur && $idJoueur <= 4);
+        
+        $q = $this->db->query("select id from cartes");
+        $indice = rand(0, $q->num_rows());
+        $i = 0;
+        foreach($q->result() as $row){
+            if($i == $indice){
+                $this->db->query("update cartes set statut='main' idMain=$idJoueur");
+                break;
+            }
+            $i++;
+        }
     }
 }
