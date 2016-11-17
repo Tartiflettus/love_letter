37cbd3c053d33cf390682ff4434175895127bd46
dalessandro <vive-les-palmiersx2@live.fr>
2016-11-17 17:45:11 +0100

    images cartes

--- script.sql
+++ script.sql
@@ -2,32 +2,37 @@ drop table Jeu;
 drop table Carte;
 drop table Joueurs;
 
-create table Jeu (
-	num_partie int primary key auto_increment,
-	manche int not null,
-	joueur_1 varchar(10) references Joueurs(nom),
-	joueur_2 varchar(10) references Joueurs(nom),
-	joueur_3 varchar(10) references Joueurs(nom),
-	joueur_4 varchar(10) references Joueurs(nom)
-);
 
 create table Joueurs (
 	id int primary key auto_increment,
-	nom varchar(10),
+	nom varchar(10)default 'toto',
 	adresse varchar(10) not null,
 	points int default 0,
 	elimine int default 0
 );
 
+create table Jeu (
+	num_partie int primary key auto_increment,
+	manche int not null,
+	joueur_1 int references Joueurs(id),
+	joueur_2 int references Joueurs(id),
+	joueur_3 int references Joueurs(id),
+	joueur_4 int references Joueurs(id),
+	joueur_actu int Joueurs(id),
+	nb_joueurs int default 0
+);
+
+
+
 create table Carte (
-	id_carte int primary key auto_increment, --id unique 0-15
+	id_carte int primary key auto_increment,
 	valeur int not null, -- rang de la carte 1-8
 	num_partie int not null references Jeu(num_partie),
-	statut varchar(10) not null default 'pioche', /*peut prendre valeur : 
+	statut varchar(10) not null default 'pioche', /*peut prendre valeurs : 
 													pioche, defausse, carte(1,2,3),
 													main, pose*/
-	main_joueur varchar(10) null references Joueurs(nom),
-	pose_joueur varchar(10) null references Joueurs(nom),
+	main_joueur varchar(10) null references Joueurs(id),
+	pose_joueur varchar(10) null references Joueurs(id),
 	image varchar(100) null
 );
 
