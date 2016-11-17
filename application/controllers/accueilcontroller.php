ebfdcc3a1fb9a04a0534a27039f85c5303fb2def
Creusel <victor.creusel4@etu.univ-lorraine.fr>
2016-11-17 18:28:05 +0100

    Configuration de la base de données

--- application/controllers/accueilcontroller.php
+++ application/controllers/accueilcontroller.php
@@ -6,17 +6,18 @@ class AccueilController extends CI_Controller {
     function __construct(){
         parent::__construct();
         $this->load->model('jeumodel');
+        $this->load->database();
     }
 
     public function index(){
-        $_SESSION["num_partie"] = $this->model->getPartie();
+        $_SESSION["num_partie"] = $this->jeumodel->getPartie();
         $data["num_partie"] = $_SESSION["num_partie"];
 
         $this->load->view("pages/index", $data);
     }
 
     public function enregistrer($nom){
-        $this->model->enregistrer($nom);
+        $this->jeumodel->enregistrer($nom);
         echo "success";
     }
 
