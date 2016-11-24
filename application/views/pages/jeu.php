c8bbfcdc1ea0b6108a377c20271d500b529fc28f
dalessandro <vive-les-palmiersx2@live.fr>
2016-11-24 14:28:19 +0100

    modif vue

--- application/views/pages/jeu.php
+++ application/views/pages/jeu.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title><?php echo $title ?></title>
-        <link rel="stylesheet" href="<?php echo base_url() ?>styles/jeu.css" />
-    </head>
-    <body>
-        <h1>Jeu</h1>
 
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title ?></title>
+    <link rel="stylesheet" href="<?php echo base_url() ?>styles/jeu.css" />
+</head>
+<body>
+    <h1>Jeu</h1>
+    <div id="bas" border="1">
+    <table>
+        
+        <tr>
+            
+            <td><img src="<?php echo base_url("images_cartes/princess.png") ?>" /></td>
+            <td><img src="<?php echo base_url("images_cartes/guard.png") ?>" /></td>
 
-    </body>
-</html>
+        </tr>
+        
+    </table>
+</div>
+    <script>
+        function poserCarte(){
+            //appeler poserCarte modele
+        }
+    </script>
+</body>
 
