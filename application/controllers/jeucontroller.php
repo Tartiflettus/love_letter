830dea7c9a5850adf9d33085ce90c9d4bd83df44
Creusel <victor.creusel4@etu.univ-lorraine.fr>
2016-11-24 17:40:24 +0100

    La vue est correctement mise à jour chaque seconde

--- application/controllers/jeucontroller.php
+++ application/controllers/jeucontroller.php
@@ -9,11 +9,6 @@ class JeuController extends CI_Controller {
     }
 
     public function view() {
-        /* if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
-          {
-          // Whoops, we don't have a page for that!
-          show_404();
-          } */
 
         $data['title'] = "jeu";
         $data["main"] = $this->jeumodel->getMain();
--- application/views/pages/jeu.php
+++ application/views/pages/jeu.php
@@ -1,97 +1,94 @@
 <!DOCTYPE html>
-
+<html>
 <head>
     <meta charset="UTF-8">
     <title><?php echo $title ?></title>
     <link rel="stylesheet" href="<?php echo base_url() ?>styles/jeu.css" />
 </head>
 <body>
-    <h1>Jeu</h1>
-    <?php
-    
-    $tailleMain = count($main);
-    $taillePose = count($pose);
-    $tailleMilieu = count($retires);
-    $tailleMain1 = count($main1);
-    
-    ?>
-    <div id="bas" border="1">
-        <table>
-            <tr>
-                <?php
-                if ($tailleMain != 0) {
-                    for ($i = 0; $i < $tailleMain; $i++) {
-                        echo '<td><img src="' . base_url($main[$i]->image) . '" name="' . $main[0]->id_carte . '" id="plop" alt="main j1"/></td>';
-                    }
-                }
-                ?>
+    <div id="maj">
+        <h1>Jeu</h1>
+        <?php
 
-            </tr>
-            <tr>
-                <?php
-                if ($taillePose != 0) {
-                    for ($i = 0; $i < $taillePose; $i++) {
-                        echo '<td><img src="' . base_url($pose[$i]->image) . '" name="' . $pose[0]->id_carte . '" alt="pose j1"/></td>';
-                    }
-                }
-                ?>
-            </tr>
-        </table>
-    </div>
-    
-    <div id="milieu" border="1">
-        <table>
-            <tr>
-                <?php
-                if ($tailleMilieu != 0) {
-                    for ($i = 0; $i < $tailleMain; $i++) {
-                        echo '<td><img src="' . base_url($retires[$i]->image) . '" name="' . $retires[0]->id_carte . '"alt="milieu"/></td>';
-                    }
-                }
-                ?>
+        $tailleMain = count($main);
+        $taillePose = count($pose);
+        $tailleMilieu = count($retires);
+        $tailleMain1 = count($main1);
 
-            </tr>
-           </table>
-    </div>
-        
-    <div id="haut" border="1">
-        <table>
-            <tr>
-                <?php
-                if ($tailleMain1 != 0) {
-                    for ($i = 0; $i < $tailleMain1; $i++) {
-                        echo '<td><img src="' . base_url($main1[$i]->image) . '" name="' . $main1[0]->id_carte . '" alt="main j2"/></td>';
+        ?>
+        <div id="bas" border="1">
+            <table>
+                <tr>
+                    <?php
+                    if ($tailleMain != 0) {
+                        for ($i = 0; $i < $tailleMain; $i++) {
+                            echo '<td><img src="' . base_url($main[$i]->image) . '" name="' . $main[0]->id_carte . '" id="plop" alt="main j1"/></td>';
+                        }
                     }
-                }
-                ?>
+                    ?>
 
-            </tr>
-        </table>
+                </tr>
+                <tr>
+                    <?php
+                    if ($taillePose != 0) {
+                        for ($i = 0; $i < $taillePose; $i++) {
+                            echo '<td><img src="' . base_url($pose[$i]->image) . '" name="' . $pose[0]->id_carte . '" alt="pose j1"/></td>';
+                        }
+                    }
+                    ?>
+                </tr>
+            </table>
+        </div>
+
+        <div id="milieu" border="1">
+            <table>
+                <tr>
+                    <?php
+                    if ($tailleMilieu != 0) {
+                        for ($i = 0; $i < $tailleMain; $i++) {
+                            echo '<td><img src="' . base_url($retires[$i]->image) . '" name="' . $retires[0]->id_carte . '"alt="milieu"/></td>';
+                        }
+                    }
+                    ?>
+
+                </tr>
+            </table>
+        </div>
+
+        <div id="haut" border="1">
+            <table>
+                <tr>
+                    <?php
+                    if ($tailleMain1 != 0) {
+                        for ($i = 0; $i < $tailleMain1; $i++) {
+                            echo '<td><img src="' . base_url($main1[$i]->image) . '" name="' . $main1[0]->id_carte . '" alt="main j2"/></td>';
+                        }
+                    }
+                    ?>
+
+                </tr>
+            </table>
+        </div>
     </div>
+
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script>
-        
-        $(document).ready(function(){
-            $("#plop").click(function(){
-                $.ajax({
-                    url : <?php echo base_url() ?>+"jeucontroller.php/testAjax", 
-                    success : function(data){
-                        console.log(data);
-                    }
-                });
-            });
+        /*
+
+        Le web est un MERDE CORDIALEMENT.
+        Les types qui ont décidé de normaliser (on peut vraiment appeler cette merde une norme ?)
+        ... le web ainsi sont des CONNARDS DE SERVICE.
+
+         */
+
+        $(document).ready(function() {
+            setInterval(function () {
+                console.log("coucou");
+                $("#maj").load("localhost/index.php/jeucontroller/view" + ' #maj');
+            }, 1000);
         });
-        
-        console.log(<?php echo base_url() ?>+"jeucontroller.php/testAjax");
-        function poserCarte() {
-            $.ajax({
-                url : <?php echo base_url() ?>+"jeucontroller.php/testAjax", 
-                success : function(data){
-                    console.log(data);
-                }
-            });
-        }
+
     </script>
 </body>
 
