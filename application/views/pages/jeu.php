b5ecbc8076ae24dd5de18fa78db083df7c2bbdc7
dalessandro <vive-les-palmiersx2@live.fr>
2016-11-24 15:11:49 +0100

    affichage main et pose joueur fonctionnel

--- application/views/pages/jeu.php
+++ application/views/pages/jeu.php
@@ -7,28 +7,46 @@
 </head>
 <body>
     <h1>Jeu</h1>
+    <?php
+    $tailleMain = count($main);
+    $taillePose = count($pose);
+    ?>
     <div id="bas" border="1">
-    <table>
-        
-        <tr>
+        <table>
+
+            <tr>
+                <?php
+                if ($tailleMain != 0) {
+                    for ($i = 0; $i < $tailleMain; $i++) {
+                        echo '<td><img src="' . base_url($main[$i]->image) . '" name="' . $main[0]->id_carte . '" onclick="poserCarte()"/></td>';
+                    }
+                }
+                ?>
+
+            </tr>
+            <tr>
+                <?php
+                if ($taillePose != 0) {
+                    for ($i = 0; $i < $taillePose; $i++) {
+                        echo '<td><img src="' . base_url($pose[$i]->image) . '" name="' . $pose[0]->id_carte . '"/></td>';
+                    }
+                }
+                ?>
+            </tr>
             
-            <td><img src="<?php echo base_url("images_cartes/princess.png") ?>" /></td>
-            <td><img src="<?php echo base_url("images_cartes/guard.png") ?>" /></td>
 
-
-        </tr>
-        
-    </table>
-</div>
+        </table>
+    </div>
     <script>
-        function poserCarte(){
+        function poserCarte() {
             //appeler poserCarte modele
+            alert("coucou");
         }
     </script>
 </body>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>g
+
 </html>
 
 
